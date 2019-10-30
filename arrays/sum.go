c5af5a1e7b2b7b9f5e42fcf2f7db641bd92373c2
David Thorpe <davidthorpe878@gmail.com>
2019-10-30 20:53:12 +0000

    Updated Sum tests

---
+++ arrays/sum.go
@@ -0,0 +1,31 @@
+package main
+
+// Sum will return sum of numbers in an array
+func Sum(numbers []int) (sum int) {
+
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+// SumAll add together numbers in slices
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails adds to together all numbers in slice apart from first number
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
---
+++ arrays/sum_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{1, 3, 7})
+	want := []int{3, 11}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+func TestSumAllTails(t *testing.T) {
+	t.Run("Make sums of some slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{1, 4, 2}, []int{2, 3})
+		want := []int{6, 3}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{}, []int{2, 3})
+		want := []int{0, 3}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
