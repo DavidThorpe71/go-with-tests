a869239982f849557af7adb9206723a65fe36b6c
David Thorpe <davidthorpe878@gmail.com>
2020-02-27 19:21:56 +0000

    Update conc

--- concurrency/conc.go
+++ concurrency/conc.go
@@ -1,14 +1,20 @@
 package concurrency
 
+// WebsiteChecker is a type
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
+// CheckWebsites is a function
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			resultChannel <- result{u wc(u)}
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
